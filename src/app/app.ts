3c5f6e88fa77f8591f9ebd56755c10246027f2aa
Rosendo Salazar <rosendosalazar@live.com>
2025-12-25 21:46:44 -0600

    push site

---
+++ src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('Swag Basketball');
+  protected readonly menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+}
